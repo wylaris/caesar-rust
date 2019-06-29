3c5ac1dd19c2e7be0ace0de31678878786028c09
wylaris <pcs7709@g.rit.edu>
2019-06-29 12:56:01 -0400

    Can read a single line of a file and contains a small test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::fs;
+
 use clap::{App, Arg, crate_version};
 
 fn main() {
     let matches = build_app().get_matches();
-    let file = matches.value_of("FILE");
-    println!("Hello, world!");
+    let file = matches.value_of("file").unwrap();
+    let contents = fs::read_to_string(file).expect("Unable to read file");
+    println!("File contents: {:?}", contents);
 }
 
 

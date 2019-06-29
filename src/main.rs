a1e68e9d3f61b14cf4bca779f5c747bba79c7c79
wylaris <pcs7709@g.rit.edu>
2019-06-29 12:59:21 -0400

    Cleaned up printing of file and added multilined test.  Time to start shifting!

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let matches = build_app().get_matches();
     let file = matches.value_of("file").unwrap();
     let contents = fs::read_to_string(file).expect("Unable to read file");
-    println!("File contents: {:?}", contents);
+    println!("File contents: \n\n{}", contents);
 }
 
 
